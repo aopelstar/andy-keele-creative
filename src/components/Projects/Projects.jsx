cc0e7bf3c21857d004d643482570b213d839da50
Andrew Keele <hiimandyk@MacBook-Pro.local>
2018-07-25 00:08:16 -0600

    updated for tko-media project

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -28,16 +28,28 @@ export default function projects() {
                     <img src="https://res.cloudinary.com/drapplestar/image/upload/v1532376964/AJK%20creative/twitterbook.png" alt="twitter photo 2" className="twitter-photo" />
                 </div>
             </div>
+            <div className="project-repo">
+                <div>
+                    <a href="https://github.com/aopelstar/Twitter-book" target="_blank">
+                    Click here to visit the repo
+                    </a>
+                    </div>
+                <div>
+                    <a href="https://www.youtube.com/embed/5_IYrQumvCk" target="_blank">
+                    Click here to watch the site tour
+                    </a>
+                    </div>
+            </div>
             <h2>
-                Timpview Events
+                TKO Media
         </h2>
             <h3>
-                A mobile first, fullstack web app
+                A full stack web application for a local sports media company
         </h3>
             <ul>
-                <li>•React and JS to create components</li>
-                <li>•Server created using Node and Express</li>
-                <li>•Data management using PostgreSQL</li>
+                <li>•React framework</li>
+                <li>•NodeMailer to send emails to client via web server</li>
+                <li>•Cloudinary API and framework </li>
             </ul>
             <div>
 
@@ -47,6 +59,18 @@ export default function projects() {
                 <div>
                     <img src="https://res.cloudinary.com/drapplestar/image/upload/v1532376967/AJK%20creative/home.png" alt="twitter photo 2" className="twitter-photo" />
                 </div>
+            </div>
+            <div className="project-repo">
+                <div>
+                    <a href="https://github.com/aopelstar/media-tko">
+                    Click here to visit the repo
+                    </a>
+                    </div>
+                    <div>
+                        <a href="http://www.tko-media.andrewkeelecreative.com" target="_blank">
+                        Click here to visit the site(still in beta)
+                        </a>
+                    </div>
             </div>
 
         </div>
